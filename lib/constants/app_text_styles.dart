ad3db6a2aa04ad3fceb7e080a8c6af70ccd0bf17
maryammasood831 <mariammasood4567@gmail.com>
2025-12-30 17:43:51 +0500

    Delete lib directory

--- lib/constants/app_text_styles.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-class AppTextStyles {
-  static TextStyle primaryStyle() {
-    return TextStyle(fontSize: 22, color: Colors.black);
-  }
-}
